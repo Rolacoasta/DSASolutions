deb738e8ab22d186c567703ae981e44693fb5378
Rolacoasta <maxamadden@yahoo.com>
2024-01-26 17:07:48 -0600

    Organized tests and solutions into folders to use pytest features such as markers and directory commands for testing.
    Example pytest tests/array_and_string/
    Example pytest -m array_and_string

--- tests/introduction/max_value_test.py
+++ tests/introduction/max_value_test.py
@@ -1,13 +1,15 @@
+import pytest
 import colorama
 from colorama import Fore, Style
-from Solutions.max_value import max_value
+from Solutions.introduction.max_value import max_value
 
 colorama.init(autoreset=True)
 
+@pytest.mark.introduction
 def test_00():
     try:
         result = max_value([4, 7, 2, 8, 10, 9])
         assert result == 10, f"Expected 10, got {result}"
-        print(Fore.GREEN + f"\nTest passed for 00, expected 10, got {result}")
+        print(Fore.GREEN + f"\n\n\nTest passed for 00, expected 10, got {result}")
     except AssertionError as e:
         print(Fore.RED + "\nTest Failed: " + str(e))
